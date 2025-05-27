99749fb495888e1c45f4db5c314ebe5019e2bf07
evanalmighty84 <Godlovesme24!!>
2025-05-27 16:15:11 -0500

    removed hashrouter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,15 @@
-// src/App.js
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/templatemo-style.css';
-
-import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HelmetProvider } from 'react-helmet-async'; // <-- ADD THIS
+import HomePage from "./components/Home";
+import Navbar from './components/Navbar'
 
 function App() {
     return (
-        <HelmetProvider>
-            <HashRouter>
-                <Home />
-            </HashRouter>
+        <HelmetProvider> {/* <-- WRAP your app here */}
+
+            <HomePage />
         </HelmetProvider>
     );
 }
